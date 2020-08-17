074e8a5db92c0a3182770345af664f5334a7ae1c
AmanSharma <amansharma.11@hotmail.com>
2020-08-18 02:21:00 +0530

    commit

--- app/src/main/java/com/example/finalchallengeday1_project4_json_apis_networkservices_parsingjson/MainActivity.java
+++ app/src/main/java/com/example/finalchallengeday1_project4_json_apis_networkservices_parsingjson/MainActivity.java
@@ -9,9 +9,12 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Method;
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
     public static final String JSON_OBJECT_URL = "https://jsonplaceholder.typicode.com/todos/1";
-    public static final String JSON_ARRAY_URL = "http://jsonplaceholder.typicode.com/todos";
+    public static final String JSON_ARRAY_URL = "https://jsonplaceholder.typicode.com/todos";
     private RequestQueue requestQueue;
 
     @Override
@@ -34,8 +37,7 @@ public class MainActivity extends AppCompatActivity {
         requestQueue = VolleySingleton.getInstance(this).getRequestQueue();
 
         //fetching json object
-
-        JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.GET, JSON_OBJECT_URL, null,
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, JSON_OBJECT_URL, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
@@ -48,6 +50,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        requestQueue.add(jsonObject);
+        //fetching json array
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, JSON_ARRAY_URL, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for (int i = 0; i<response.length(); i++){
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                Log.d(TAG, "onResponse: "+jsonObject.getString("id")+"---"+jsonObject.getBoolean("completed"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse: "+error.getMessage());
+            }
+        });
+//        requestQueue.add(jsonObjectRequest);
+        requestQueue.add(jsonArrayRequest);
     }
 }
